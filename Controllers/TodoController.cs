bef8b9070f9f9424dafa49cd029b02a9e31a2d7c
hgirish <girish@post.com>
2017-11-28 23:03:31 -0800

    Added Todo View

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -1,11 +1,15 @@
+using AspNetCoreTodo.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace AspNetCoreTodo.Controllers
 {
     public class TodoController : Controller
     {
         public IActionResult Index(){
-            return Content("Hello");
+            var items = new List<TodoItem>();
+            var viewModel = new TodoViewModel { Items = items };
+            return View(viewModel);
         }
     }
 }
