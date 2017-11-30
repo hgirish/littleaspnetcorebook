0b508aa45f224898467332d1962fd6d6f9de7777
hgirish <girish@post.com>
2017-11-29 20:55:50 -0800

    Added Integration Tests

---
+++ AspNetCoreTodo.IntegrationTests/TodoRouteShould.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AspNetCoreTodo.IntegrationTests
+{
+   public class TodoRouteShould : IClassFixture<TestFixture>
+    {
+        private readonly HttpClient _client;
+        public TodoRouteShould(TestFixture fixture)
+        {
+            _client = fixture.Client;
+        }
+        [Fact]
+        public async Task ChallengeAnonumouseUser()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "/todo");
+
+            var response = await _client.SendAsync(request);
+
+            Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
+            Assert.Equal("http://localhost:5000/Account/Login?ReturnUrl=%2Ftodo",
+                response.Headers.Location.ToString());
+
+        }
+            
+    }
+}
